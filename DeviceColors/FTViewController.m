0688c15170f158c1c5eea8b53a0ba6600c6b84ff
Ortwin Gentz <gentz@futuretap.com>
2013-10-11 17:01:03 +0200

    Initial commit

--- DeviceColors/FTViewController.m
+++ DeviceColors/FTViewController.m
@@ -7,8 +7,18 @@
 //
 
 #import "FTViewController.h"
+#import <QuartzCore/QuartzCore.h>
+#include <sys/utsname.h>
 
 @interface FTViewController ()
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *horizontalSpaceConstraint;
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet UIView *labelContainerView;
+@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *modelLabel;
+@property (strong, nonatomic) IBOutlet UILabel *deviceColorLabel;
+@property (strong, nonatomic) IBOutlet UIWebView *deviceColorWebView;
+@property (strong, nonatomic) IBOutlet UIWebView *deviceEnclosureColorWebView;
 
 @end
 
@@ -18,12 +28,75 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	self.horizontalSpaceConstraint.constant = UIInterfaceOrientationIsPortrait(self.interfaceOrientation) ? 40 -self.view.frame.size.width : 20;
+
+
+	
+	self.deviceColorWebView.scrollView.scrollEnabled = NO;
+	self.deviceColorWebView.layer.borderColor = [UIColor grayColor].CGColor;
+	self.deviceColorWebView.layer.borderWidth = 1;
+
+	self.deviceEnclosureColorWebView.scrollView.scrollEnabled = NO;
+	self.deviceEnclosureColorWebView.layer.borderColor = [UIColor grayColor].CGColor;
+	self.deviceEnclosureColorWebView.layer.borderWidth = 1;
+	
+	UIDevice *device = [UIDevice currentDevice];
+	NSString *modelCode = [self hardwareModel];
+	NSString *modelName = @"";
+	
+	SEL selector = NSSelectorFromString([device.systemVersion hasPrefix:@"7"] ? @"_deviceInfoForKey:" :  @"deviceInfoForKey:");
+	NSString *deviceColor = [device performSelector:selector withObject:@"DeviceColor"];
+	NSString *deviceEnclosureColor = [device performSelector:selector withObject:@"DeviceEnclosureColor"];
+	NSString *color = deviceEnclosureColor && ![deviceEnclosureColor isEqualToString:@"unknown"] ? deviceEnclosureColor : deviceColor;
+	color = [color stringByReplacingOccurrencesOfString:@"#" withString:@""];
+	
+	NSArray *models = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Info" ofType:@"plist"]][@"UTExportedTypeDeclarations"];
+	for (NSDictionary *modelDict in models) {
+		if ([(NSArray*)modelDict[@"UTTypeTagSpecification"][@"com.apple.device-model-code"] containsObject:modelCode]) {
+			if ([modelDict[@"UTTypeIdentifier"] hasSuffix:color]) {
+				NSString *imageName = [modelDict[@"UTTypeIconFile"] stringByReplacingOccurrencesOfString:@".icns" withString:@".jpg"];
+				self.imageView.image = [UIImage imageNamed:imageName];
+				
+				modelName = modelDict[@"UTTypeDescription"];
+			}
+		}
+	}
+
+	self.nameLabel.text = modelName;
+	self.modelLabel.text = modelCode;
+
+	NSString *htmlStringFormat = @"<html><body style=\"margin: 5px; font: 13px '%@', Helvetica; background-color: %@; color: %@;\">%@</body></html>";
+	
+	NSString *fontName = self.deviceColorLabel.font.fontName;
+	
+	NSArray *darkColors = @[@"black", @"#3b3b3c", @"#99989b"];
+	NSString *deviceForegroundColor = [darkColors containsObject:deviceColor] ? @"white" : @"black";
+	NSString *deviceColorHTML = [NSString stringWithFormat:htmlStringFormat, fontName, deviceColor, deviceForegroundColor, deviceColor];
+	
+	NSString *deviceEnclosureForegroundColor = [darkColors containsObject:deviceEnclosureColor] ? @"white" : @"black";
+	NSString *deviceEnclosureColorHTML = [NSString stringWithFormat:htmlStringFormat, fontName, deviceEnclosureColor, deviceEnclosureForegroundColor, deviceEnclosureColor];
+	
+	[self.deviceColorWebView loadHTMLString:deviceColorHTML baseURL:nil];
+	[self.deviceEnclosureColorWebView loadHTMLString:deviceEnclosureColorHTML baseURL:nil];
+	
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+	self.horizontalSpaceConstraint.constant = UIInterfaceOrientationIsPortrait(toInterfaceOrientation) ? 40 -self.view.frame.size.width : 20;
+}
+
+- (NSString*)hardwareModel {
+	struct utsname u;
+
+	uname(&u);
+	
+	return @(u.machine);
 }
 
 @end
